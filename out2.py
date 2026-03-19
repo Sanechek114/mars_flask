e8a0592908388ab7a25d6930bfbb46e06ddee003
aleksander <ermolaevsasa114@gmail.com>
2026-03-19 17:20:24 +0300

    все

--- out2.py
+++ out2.py
@@ -11,4 +11,4 @@ db_sess = create_session()
 for user in db_sess.query(User).filter(User.address.like("%module_1%"), 
                                        User.speciality.notilike("%engineer%"), 
                                        User.position.notilike("%engineer%")):
-    print(user)
+    print(user.id)
